cd4bb0f111ae5ec46a0711f115c4bbc3b4d60747
Sai Prakash Pulipaka <pulipakasaiprakash@gmail.com>
2026-07-12 14:01:56 +0530

    Update pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b88ef98966503566"; }
+/******/ 		__webpack_require__.h = function() { return "0f1ba4abfaf32bae"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
